f01747935a67e19bb13118e42c7c4bed94219bb6
Abraham Chen <Abraham_Chen@kingston.com.tw>
2021-01-22 13:17:35 +0800

    add sqlite curd project

---
+++ WPF.NET5.SQLite.CRUD/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WPF.NET5.SQLite.CRUD.Data;
+
+namespace WPF.NET5.SQLite.CRUD
+{
+    public partial class App : Application
+    {
+        #region Private members
+        private readonly ServiceProvider serviceProvider;
+        #endregion
+        #region Constructor
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<ProductDbContext>(options =>
+            {
+                options.UseSqlite("Data Source = Product.db");
+            });
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+        #endregion
+        #region Event Handlers
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+        #endregion
+    }
+}
---
+++ WPF.NET5.SQLite.CRUD/Data/ProductDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace WPF.NET5.SQLite.CRUD.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        #region Constructor
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        #endregion
+
+        #region Public properties
+        public DbSet<Product> Products { get; set; }
+        #endregion
+
+        #region Overridden methods
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(GetProducts());
+            base.OnModelCreating(modelBuilder);
+        }
+        #endregion
+
+        #region Private methods
+        private Product[] GetProducts()
+        {
+            return new Product[]
+            {
+                new Product { Id = 1, Name = "TShirt", Description = "Blue Color", Price = 2.99, Unit =1},
+                new Product { Id = 2, Name = "Shirt", Description = "Formal Shirt", Price = 12.99, Unit =1},
+                new Product { Id = 3, Name = "Socks", Description = "Wollen", Price = 5.00, Unit =2},
+                new Product { Id = 4, Name = "Tshirt", Description = "Red", Price = 2.99, Unit =3},
+            };
+        }
+        #endregion
+    }
+}
