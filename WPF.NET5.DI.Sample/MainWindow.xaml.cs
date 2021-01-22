b8425e285e0e6fa3dfb605921a36f1b416203a25
Abraham Chen <Abraham_Chen@kingston.com.tw>
2021-01-22 12:29:29 +0800

    Add project files.

---
+++ WPF.NET5.DI.Sample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF.NET5.DI.Sample.Data;
+
+namespace WPF.NET5.DI.Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EmployeeDbContext dbContext;
+        public MainWindow(EmployeeDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            InitializeComponent();
+            GetEmployees();
+        }
+        private void GetEmployees()
+        {
+            var employees = dbContext.Employee.ToList();
+            EmployeeDG.ItemsSource = employees;
+        }
+    }
+}
